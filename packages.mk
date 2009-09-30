95c8f770f6193c6c517e9e22ea54ef52e81a8b42
tomytsai <none@example.com>
2009-09-30 00:49:06 +0000

    *** empty log message ***

--- packages.mk
+++ packages.mk
@@ -2,5 +2,5 @@
 packages := service collection xtc 
 packages += config mon vmon
 packages += utility management client 
-#packages += camera acqiris evgr epicsArch
-packages += camera evgr epicsArch rceProxy
+packages += camera acqiris evgr epicsArch rceProxy
+
