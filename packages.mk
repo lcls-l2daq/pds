81351c5a68fa5d2f10be8f2a802ae0477107f13f
caf <none@example.com>
2009-10-08 04:39:56 +0000

    add offlineclient to packages

--- packages.mk
+++ packages.mk
@@ -1,6 +1,6 @@
 # List of packages (low level first)
 packages := service collection xtc 
 packages += config mon vmon
-packages += utility management client 
+packages += utility management client offlineclient
 packages += camera acqiris evgr epicsArch rceProxy
 
