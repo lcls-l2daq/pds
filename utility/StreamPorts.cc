fc2ec27f6ea2dc9686df617a17b842e3e9792da4
weaver <none@example.com>
2009-11-20 22:11:17 +0000

    *** empty log message ***

--- utility/StreamPorts.cc
+++ utility/StreamPorts.cc
@@ -5,30 +5,29 @@ using namespace Pds;
 //
 //  Ranges assume the following limits:
 //    Max Partitions       = 16
-//    Max Level2/partition = 64
-//    Max Level3/partition = 16
+//    Max Level2/partition = 32
 //
-static const int MaxPartitionL1s = 64;
+static const int MaxPartitionL1s = 32;
 static const int MaxPartitionL2s = 64;
 static const int MaxBLDServers   = 32;
 static const int BaseMcastAddr = 0xefff1100;
 
-// EVR -> L1 : 0xefff1110
-static const int SegmentMcastAddr  = BaseMcastAddr    +StreamPorts::MaxPartitions; 
-// L1  -> L2 : 0xefff1120
+// EVR -> L1 : 0xefff1100
+static const int SegmentMcastAddr  = BaseMcastAddr;
+// L1  -> L2 : 0xefff1110
 static const int EventMcastAddr    = SegmentMcastAddr +StreamPorts::MaxPartitions;
-// L2  -> L0 : 0xefff1520
+// L2  -> L0 : 0xefff1510
 static const int ControlMcastAddr  = EventMcastAddr   +StreamPorts::MaxPartitions*MaxPartitionL2s;
-// VMON server<->client // 0xeffff1530
+// VMON server<->client // 0xeffff1520
 static const int VmonMcastAddr     = ControlMcastAddr +StreamPorts::MaxPartitions;
 // BLD -> L1,L2 : 0xefff1800
 static const int BLDMcastAddr      = 0xefff1800;  // FIXED value for external code
 
-static const unsigned PortBase         = 10002;
-static const unsigned SegmentPortBase  = PortBase;                           // 10002
-static const unsigned EventPortBase    = SegmentPortBase+1;                  // 10003
-static const unsigned ControlPortBase  = EventPortBase   +MaxPartitionL1s;   // 10067
-static const unsigned VmonPortBase     = ControlPortBase +MaxPartitionL2s;   // 10131
+static const unsigned PortBase         = 10150;
+static const unsigned SegmentPortBase  = PortBase;                                                    // 10150
+static const unsigned EventPortBase    = SegmentPortBase +StreamPorts::MaxPartitions;                 // 10166
+static const unsigned ControlPortBase  = EventPortBase   +StreamPorts::MaxPartitions*MaxPartitionL1s; // 10678
+static const unsigned VmonPortBase     = ControlPortBase +StreamPorts::MaxPartitions*MaxPartitionL2s; // 11702
 static const unsigned BLDPortBase      = 10148;   // FIXED value for external code
 
 
