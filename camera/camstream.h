8033d80ae3942ffe32b5b7ad8dde8f28be4d6b2b
jtiller <none@example.com>
2010-03-12 01:58:51 +0000

    Used #pragma to pack the structure along 4-byte boundaries.  Important when passing data between different-sized architectures (32- vs. 64-bit).
    Modified frame header to use an offset as opposed to a pointer for the
    data member.  Pointers don't play well in structures that might travel
    across system boundaries.
    
    Reordered the declarations.

--- camera/camstream.h
+++ camera/camstream.h
@@ -11,24 +11,29 @@
 #define CAMSTREAM_DEFAULT_PORT		10000
 #define CAMSTREAM_PKT_MAXSIZE		(32*1024)
 
+#pragma pack(4)
 struct camstream_basic_t {
 	uint32_t hdr;
 	uint32_t pktsize;
 };
-#define CAMSTREAM_BASIC_HDR		htonl(0)
-#define CAMSTREAM_BASIC_PKTSIZE		htonl(sizeof(struct camstream_basic_t))
 
 struct camstream_image_t {
 	struct camstream_basic_t base;
 	uint8_t format;
-#define IMAGE_FORMAT_UNKNOWN	0
-#define IMAGE_FORMAT_GRAY8	1
-#define IMAGE_FORMAT_GRAY16	2
 	uint32_t size;
 	uint16_t width;
 	uint16_t height;
-	void *data;	
+	uint32_t data_off;
 };
+#pragma pack()
+
+#define CAMSTREAM_BASIC_HDR		htonl(0)
+#define CAMSTREAM_BASIC_PKTSIZE		htonl(sizeof(struct camstream_basic_t))
+
+#define IMAGE_FORMAT_UNKNOWN	0
+#define IMAGE_FORMAT_GRAY8	1
+#define IMAGE_FORMAT_GRAY16	2
+
 #define CAMSTREAM_IMAGE_HDR		htonl(('I'<<24) | ('M'<<16) | ('G'<< 8) | 0)
 #define CAMSTREAM_IMAGE_PKTSIZE		htonl(sizeof(struct camstream_image_t))
 
