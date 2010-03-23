79925f7b1df7a6302768775e35d67806b963d4a9
tomytsai <none@example.com>
2010-03-23 19:49:19 +0000

    Commit Matt's update for support Princeton's NetDgServer in the segment level

--- utility/EbCountKey.hh
+++ utility/EbCountKey.hh
@@ -5,6 +5,7 @@
 
 #include "EvrServer.hh"
 #include "EbCountSrv.hh"
+#include "EbSequenceSrv.hh"
 
 #include "pds/service/Pool.hh"
 
@@ -29,6 +30,10 @@ namespace Pds {
     virtual bool precedes (const EvrServer& s) { return key <= s.count(); }
     virtual bool coincides(const EvrServer& s) { return key == s.count(); }
     virtual void assign   (const EvrServer& s) { key = s.count(); seq = s.sequence(); }
+    //  Special case for only one server providing timestamp
+    virtual bool precedes (const EbSequenceSrv& s) { return false; }
+    virtual bool coincides(const EbSequenceSrv& s) { return false; }
+    virtual void assign   (const EbSequenceSrv& s) { seq = s.sequence(); }
   public:
     const Sequence& sequence() const { return seq; }
   public:
