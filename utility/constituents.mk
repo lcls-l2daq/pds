eeee09274ca551263d1a51123895390509333919
tomytsai <none@example.com>
2011-02-04 19:01:06 +0000

    *** empty log message ***

--- utility/constituents.mk
+++ utility/constituents.mk
@@ -1,6 +1,11 @@
 libnames := utility
 
-#CXXFLAGS += -DBUILD_PRINCETON -DBUILD_PACKAGE_SPACE
-CXXFLAGS += -DBUILD_PRINCETON
+#############################
+# -DBUILD_PRINCETON      for increasing event/transition timeouts
+# -DBUILD_PACKAGE_SPACE  for solving older switch problem, not used for now
+#############################
+
+#CXXFLAGS += -DBUILD_PRINCETON -DBUILD_PACKAGE_SPACE 
+#CXXFLAGS += -DBUILD_PRINCETON
 
 libsrcs_utility := $(wildcard *.cc)
