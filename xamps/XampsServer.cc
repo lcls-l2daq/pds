8d1069764af0f440d91a54e0ced4189b82c9069d
no author <no_author@nowhere.com>
2011-07-20 21:30:57 +0000

    This commit was manufactured by cvs2svn to create tag 'V02-12-00'.

--- xamps/XampsServer.cc
+++ xamps/XampsServer.cc
@@ -239,8 +239,7 @@ int Pds::XampsServer::fetch( char* payload, int flags ) {
      _xtc.damage.increase(Pds::Damage::UserDefined);
      _xtc.damage.userBits(damageMask);
      printf("XampsServer::fetch setting user damage 0x%x", damageMask);
-     if (pgpCardRx.lengthErr) printf(", rxSize(%u), maxSize(%u) ret(%d) offset(%u) (bytes)",
-         (unsigned)pgpCardRx.rxSize*sizeof(uint32_t), _payloadSize, ret, offset);
+     if (pgpCardRx.lengthErr) printf(", rxSize(%u), maxSize(%u bytes)", (unsigned)pgpCardRx.rxSize*sizeof(uint32_t), _payloadSize);
      printf("\n");
    } else {
      unsigned oldCount = _count;
