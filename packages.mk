acb809329545fa8bed93c1546ca882385c205b5f
caf <none@example.com>
2011-10-03 20:54:21 +0000

    Added Gsc16ai detector

--- packages.mk
+++ packages.mk
@@ -3,5 +3,5 @@ packages := service collection xtc
 packages += config mon vmon
 packages += utility management client offlineclient
 packages += ipimb encoder camera acqiris evgr epicsArch rceProxy \
-            princeton epicstools pgp cspad xamps
+            princeton epicstools pgp cspad xamps       gsc16ai
 
