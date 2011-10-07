845d4215372f80e868bfa3035fb825429bd18bdc
jackp <none@example.com>
2011-10-07 22:12:03 +0000

    Fri Oct  7 14:57:15 PDT 2011 jackp Latest update to fexamps

--- packages.mk
+++ packages.mk
@@ -3,5 +3,5 @@ packages := service collection xtc
 packages += config mon vmon
 packages += utility management client offlineclient
 packages += ipimb encoder camera acqiris evgr epicsArch rceProxy \
-            princeton epicstools pgp cspad xamps       gsc16ai
+            princeton epicstools pgp cspad xamps fexamp gsc16ai
 
