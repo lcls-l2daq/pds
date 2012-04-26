45e93d6b112965074d41d9dc3bde7b6d4b97c068
tomytsai <none@example.com>
2012-04-26 17:50:12 +0000

    *** empty log message ***

--- management/SegStreams.cc
+++ management/SegStreams.cc
@@ -13,7 +13,7 @@
 using namespace Pds;
 
 #ifdef BUILD_LARGE_STREAM_BUFFER
-static const unsigned MaxSize = 3<<23;
+static const unsigned MaxSize = 5<<23;
 #else
 static const unsigned MaxSize = 1<<23;
 #endif
