6cd49b612de62fc573ffbe91b863dcc118417c14
weaver <none@example.com>
2012-09-25 23:32:51 +0000

    Update control config type to V2

--- config/ControlConfigType.hh
+++ config/ControlConfigType.hh
@@ -2,9 +2,9 @@
 #define Pds_ControlConfigType_hh
 
 #include "pdsdata/xtc/TypeId.hh"
-#include "pdsdata/control/ConfigV1.hh"
+#include "pdsdata/control/ConfigV2.hh"
 
-typedef Pds::ControlData::ConfigV1 ControlConfigType;
+typedef Pds::ControlData::ConfigV2 ControlConfigType;
 
 static Pds::TypeId _controlConfigType(Pds::TypeId::Id_ControlConfig,
 				      ControlConfigType::Version);
