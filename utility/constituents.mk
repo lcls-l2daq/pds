ab5f02bb3b662d0b59017aa77456c5ac0f6e8002
tomytsai <none@example.com>
2012-09-28 19:31:23 +0000

    *** empty log message ***

--- utility/constituents.mk
+++ utility/constituents.mk
@@ -5,8 +5,7 @@ libnames := utility
 # -DBUILD_PACKAGE_SPACE  for solving older switch problem, not used for now
 #############################
 
-CXXFLAGS += -DBUILD_READOUT_GROUP -DBUILD_PRINCETON -DBUILD_PACKAGE_SPACE # for princeton camera and the switch problem
-#CXXFLAGS += -DBUILD_PRINCETON      # for princeton camera
-#CXXFLAGS += -DBUILD_READOUT_GROUP  # for running devices with different readout rate
+#CXXFLAGS += -DBUILD_READOUT_GROUP -DBUILD_PRINCETON -DBUILD_PACKAGE_SPACE # for princeton camera and the switch problem
+CXXFLAGS += -DBUILD_READOUT_GROUP  # for running devices with different readout rate
 
 libsrcs_utility := $(wildcard *.cc)
