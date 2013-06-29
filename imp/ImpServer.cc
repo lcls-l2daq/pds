37efb407cc711fc67822cd413803ea2fa9997815
jackp <none@example.com>
2013-06-29 02:21:46 +0000

    Fri Jun 28 19:21:25 PDT 2013 jackp commented out the ungates again.

--- imp/ImpServer.cc
+++ imp/ImpServer.cc
@@ -128,7 +128,7 @@ void Pds::ImpServer::enable() {
   _ignoreFetch = false;
   _firstFetch = true;
   _pgp->writeRegister(&d, Pds::Imp::enableTriggersAddr, Pds::Imp::enable);
-  _pgp->writeRegister(&d, Pds::Imp::unGateTriggersAddr, Pds::Imp::enable);
+//  _pgp->writeRegister(&d, Pds::Imp::unGateTriggersAddr, Pds::Imp::enable);
   flushInputQueue(fd());
   if (_debug & 0x20) printf("ImpServer::enable\n");
   _ignoreFetch = false;
@@ -138,7 +138,7 @@ void Pds::ImpServer::disable(bool flush) {
   _ignoreFetch = true;
   Pds::Imp::ImpDestination d;
   d.dest(Pds::Imp::ImpDestination::CommandVC);
-  _pgp->writeRegister(&d, Pds::Imp::unGateTriggersAddr, Pds::Imp::disable);
+//  _pgp->writeRegister(&d, Pds::Imp::unGateTriggersAddr, Pds::Imp::disable);
   _pgp->writeRegister(&d, Pds::Imp::enableTriggersAddr, Pds::Imp::disable);
   if (flush) flushInputQueue(fd());
   if (usleep(10000)<0) perror("ImpServer::disable ulseep 1 failed\n");
@@ -233,7 +233,7 @@ int Pds::ImpServer::fetch( char* payload, int flags ) {
      _xtc.damage.userBits(damageMask);
      printf("ImpServer::fetch setting user damage 0x%x", damageMask);
      if (pgpCardRx.lengthErr) printf(", rxSize(%u), maxSize(%u) ret(%d) offset(%u) (bytes)",
-         (unsigned)pgpCardRx.rxSize*sizeof(uint32_t), (unsigned)_payloadSize, ret, offset);
+         (unsigned int)(long unsigned int)(pgpCardRx.rxSize*sizeof(uint32_t)), (unsigned)_payloadSize, ret, offset);
      printf("\n");
    } else {
      unsigned oldCount = _count;
