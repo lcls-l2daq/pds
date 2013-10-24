aff1d69188817b74b79a74a7d891bceb9bb39411
weaver <none@example.com>
2013-10-24 21:40:43 +0000

    Fix to pnccd config construction

--- config/pnCCDConfigType.cc
+++ config/pnCCDConfigType.cc
@@ -3,13 +3,11 @@
 void Pds::pnCCDConfig::setNumLinks( pnCCDConfigType& c,
                                     const std::string& fname )
 {
-  unsigned _numLinks;
-
   FILE* fp = ::fopen(fname.c_str(), "r");
   size_t ret;
   if (fp) {
     printf("Reading pnCCD config file\n");
-    ret = fread(&_numLinks, sizeof(pnCCDConfigType), 1, fp);
+    ret = fread(&c, sizeof(pnCCDConfigType), 1, fp);
     if (ret != 1) {
       printf("Error reading pnCCD config file\n");
       throw std::string("Error reading pnCCD config file") ;
@@ -18,6 +16,4 @@ void Pds::pnCCDConfig::setNumLinks( pnCCDConfigType& c,
     perror("Could not open pnCCD file\n");
     throw std::string("Could not open pnCCD file\n");
   }
-
-  *reinterpret_cast<uint32_t*>(&c) = _numLinks;
 }
