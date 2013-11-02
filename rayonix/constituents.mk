98d3238a61dd5104c434cc1a234bad13f945e4d3
jana <none@example.com>
2013-11-02 00:14:15 +0000

    Initial check in of rayonix

---
+++ rayonix/constituents.mk
@@ -0,0 +1,5 @@
+libnames := rayonix
+
+libsrcs_rayonix := RayonixManager.cc  RayonixServer.cc RayonixOccurrence.cc rayonix_control.cc rayonix_data.cc
+
+libincs_rayonix += pdsdata/include ndarray/include
