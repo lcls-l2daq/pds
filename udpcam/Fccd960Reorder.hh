583b5038b446e36ff3aa0f8e52f9b694d403eaf1
no author <no_author@nowhere.com>
2014-03-20 00:48:46 +0000

    This commit was manufactured by cvs2svn to create tag 'V07-04-06'.

--- udpcam/Fccd960Reorder.hh
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#ifndef _FCCD960REORDER_H
-#define _FCCD960REORDER_H
-
-void fccd960Initialize(uint16_t* chanMap, uint16_t* topBot);
-void fccd960Reorder(const uint16_t* chanMap, const uint16_t* topBot, unsigned char* in, uint16_t* out);
-
-#endif
