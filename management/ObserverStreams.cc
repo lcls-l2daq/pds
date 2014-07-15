4b95d5d0405919a3330a1dce16b90b35ef5b2aa2
weaver <none@example.com>
2014-07-15 22:34:41 +0000

    Add monshmserver to vmon

--- management/ObserverStreams.cc
+++ management/ObserverStreams.cc
@@ -3,9 +3,10 @@
 #include "pds/utility/ObserverEb.hh"
 #include "pds/service/BitList.hh"
 #include "pds/management/CollectionObserver.hh"
-#include "pds/service/VmonSourceId.hh"
-//#include "VmonAppliance.hh"
+#include "pds/management/VmonServerAppliance.hh"
 #include "pds/management/EventBuilder.hh"
+#include "pds/service/VmonSourceId.hh"
+#include "pds/vmon/VmonEb.hh"
 #include "pds/xtc/XtcType.hh"
 
 #include <sys/types.h>  // required for kill
@@ -21,25 +22,25 @@ ObserverStreams::ObserverStreams(CollectionObserver& cmgr,
   const Node& node = cmgr.header();
   Level::Type level = node.level();
   int ipaddress = node.ip();
+  const Src& src = cmgr.header().procInfo();
 
-  //  VmonEb vmoneb(vmon());
+  for (int s = 0; s < StreamParams::NumberOfStreams; s++) {
 
-   for (int s = 0; s < StreamParams::NumberOfStreams; s++) {
+    _outlets[s] = new OpenOutlet(*stream(s)->outlet());
 
-     _outlets[s] = new OpenOutlet(*stream(s)->outlet());
-
-     //ObserverEb* eb = new ObserverEb
-     SegEventBuilder* eb = new SegEventBuilder
+    //ObserverEb* eb = new ObserverEb
+    SegEventBuilder* eb = new SegEventBuilder
       (cmgr.header().procInfo(),
        _xtcType,
        level,
        *stream(s)->inlet(), *_outlets[s], s, ipaddress,
-       max_size, ebdepth, slowEb);
+       max_size, ebdepth, slowEb,
+       new VmonEb(src,32,ebdepth,(1<<23),max_size));
 
     _inlet_wires[s] = eb;
+
+    (new VmonServerAppliance(src))->connect(stream(s)->inlet());
   }
-  //  _vmom_appliance = new VmonAppliance(vmon());
-  //  _vmom_appliance->connect(stream(StreamParams::Occurrence)->inlet());
 }
 
 ObserverStreams::~ObserverStreams()
