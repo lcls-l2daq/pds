d3aca811f01bf8ab2f7a7ba50212a48204a3fe62
weaver <none@example.com>
2014-08-06 19:06:07 +0000

    Move CmdLineTools to pds/service

--- service/constituents.mk
+++ service/constituents.mk
@@ -3,3 +3,4 @@ libnames := service
 ignore_src := BitMaskArray.cc RingPool.cc RingPoolW.cc KStream.cc TStream.cc
 
 libsrcs_service := $(filter-out $(ignore_src),$(wildcard *.cc))
+libincs_service := pdsdata/include ndarray/include
