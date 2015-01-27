cacdeb12a0ab282cb42633f98cc00c3583c88ac2
caf <caf@SLAC.STANFORD.EDU>
2015-01-27 18:46:00 +0000

    add reportTotals() method

--- management/RunAllocator.hh
+++ management/RunAllocator.hh
@@ -11,6 +11,8 @@ namespace Pds {
     virtual unsigned alloc() {return 0;}
     virtual int reportOpenFile(int, int, int, int, std::string&, std::string&) {return 0;}
     virtual int reportDetectors(int, int, std::vector<std::string>&) {return 0;}
+    virtual int reportTotals(int, int, long, long, double) {return 0;};
+
     virtual ~RunAllocator() {};
     enum {Error=0xffffffff};
   };
