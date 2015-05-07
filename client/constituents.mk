6d5e1df1baaca897e1641916e088b85e0ee8c3aa
weaver <weaver@SLAC.STANFORD.EDU>
2015-05-07 00:33:45 +0000

    Added L3F test binary for xtc readback

--- client/constituents.mk
+++ client/constituents.mk
@@ -5,3 +5,10 @@ libincs_client := pdsdata/include ndarray/include boost/include
 libnames += clientcompress
 libsrcs_clientcompress := FrameCompApp.cc
 libincs_clientcompress := pdsdata/include ndarray/include boost/include 
+
+tgtnames := l3ftest
+tgtsrcs_l3ftest := l3ftest.cc
+tgtlibs_l3ftest := pdsdata/xtcdata pdsdata/appdata pdsdata/psddl_pdsdata
+tgtlibs_l3ftest += pds/client pds/utility pds/service pds/collection pds/vmon pds/mon pds/xtc
+tgtslib_l3ftest := $(USRLIBDIR)/rt $(USRLIBDIR)/dl
+tgtincs_l3ftest := pdsdata/include
---
+++ client/l3ftest.cc
@@ -0,0 +1,172 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dlfcn.h>
+
+#include "pds/client/L3FilterDriver.hh"
+#include "pds/client/L3FilterThreads.hh"
+#include "pds/utility/Inlet.hh"
+#include "pds/utility/Outlet.hh"
+#include "pds/utility/OpenOutlet.hh"
+#include "pds/service/GenericPoolW.hh"
+#include "pds/xtc/CDatagram.hh"
+#include "pdsdata/app/L3FilterModule.hh"
+#include "pdsdata/xtc/XtcFileIterator.hh"
+#include "pdsdata/xtc/ProcInfo.hh"
+#include "pdsdata/xtc/L1AcceptEnv.hh"
+
+namespace Pds {
+  class MyOutlet : public Appliance {
+  public:
+    MyOutlet(Semaphore& sem) : _sem(sem), _pass(0), _fail(0) {}
+  public:
+    Transition* transitions(Transition* in) { return 0; }
+    InDatagram* events     (InDatagram* in) {
+      CDatagram* cdg = reinterpret_cast<CDatagram*>(in);
+      Datagram* dg = &cdg->datagram();
+      if (dg->seq.service()==TransitionId::L1Accept) {
+        L1AcceptEnv* env = reinterpret_cast<L1AcceptEnv*>(&dg->env);
+        if (env->l3t_result()==L1AcceptEnv::Pass)
+          _pass++;
+        if (env->l3t_result()==L1AcceptEnv::Fail)
+          _fail++;
+      }
+      else
+        printf("%s transition: time %08x/%08x  stamp %08x/%08x, dmg %08x, payloadSize 0x%x  pass:fail=%d:%d\n",
+               TransitionId::name(dg->seq.service()),
+               dg->seq.clock().seconds(),dg->seq.clock().nanoseconds(),
+               dg->seq.stamp().fiducials(),dg->seq.stamp().ticks(),
+               dg->xtc.damage.value(),
+               dg->xtc.sizeofPayload(),
+               _pass,_fail);
+      _sem.give();
+      return in;
+    }
+  private:
+    Semaphore& _sem;
+    unsigned _pass,_fail;
+  };
+};
+
+static void load_filter(char*       arg,
+                        Appliance*& apps)
+{
+  {
+    std::string p(arg);
+
+    unsigned n=0;
+    size_t posn = p.find(",");
+    if (posn!=std::string::npos) {
+      n = strtoul(p.substr(posn+1).c_str(),NULL,0);
+      p = p.substr(0,posn);
+    }
+        
+    printf("dlopen %s\n",p.c_str());
+
+    void* handle = dlopen(p.c_str(), RTLD_LAZY);
+    if (!handle) {
+      printf("dlopen failed : %s\n",dlerror());
+    }
+    else {
+      // reset errors
+      const char* dlsym_error;
+      dlerror();
+
+      // load the symbols
+      create_m* c_user = (create_m*) dlsym(handle, "create");
+      if ((dlsym_error = dlerror())) {
+        fprintf(stderr,"Cannot load symbol create: %s\n",dlsym_error);
+      }
+      else {
+        L3FilterThreads* driver = new L3FilterThreads(c_user, n);
+        if (apps != NULL)
+          driver->connect(apps);
+        else
+          apps = driver;
+      }
+    }
+  }
+}
+
+void usage(char* progname) {
+  fprintf(stderr,"Usage: %s -f <filename> -d <damage mask> [-h]\n", progname);
+}
+
+int main(int argc, char* argv[]) {
+  int c;
+  char* xtcname=0;
+  char* l3fname=0;
+  int parseErr = 0;
+  unsigned damage = -1;
+  Appliance*  apps = 0;
+
+  while ((c = getopt(argc, argv, "hf:F:x:")) != -1) {
+    switch (c) {
+    case 'h':
+      usage(argv[0]);
+      exit(0);
+    case 'x':
+      xtcname = optarg;
+      break;
+    case 'f':
+      l3fname = optarg;
+      break;
+    case 'F':
+      load_filter(optarg,apps);
+      break;
+    default:
+      parseErr++;
+    }
+  }
+  
+  if (!xtcname) {
+    usage(argv[0]);
+    exit(2);
+  }
+
+  int fd = open(xtcname,O_RDONLY | O_LARGEFILE);
+  if (fd < 0) {
+    printf("Unable to open file %s\n",xtcname);
+    exit(2);
+  }
+
+  GenericPoolW pool(0x900000,4);
+
+  Semaphore sem(Semaphore::EMPTY);
+
+  Inlet* inlet = new Inlet;
+  Outlet* outlet = new Outlet;
+  new OpenOutlet(*outlet);
+  outlet->connect(inlet);
+  (new MyOutlet(sem))->connect(inlet);
+
+  if (apps)
+    apps->connect(inlet);
+
+  Allocation alloc("l3ftest",
+                   "nodb",
+                   0);
+  alloc.set_l3t(l3fname);
+
+  Allocate* a = new(&pool) Allocate(alloc);
+  inlet->post(a);
+
+  XtcFileIterator iter(fd,0x900000);
+  Dgram* dg;
+  unsigned long long bytes=0;
+  unsigned events=0;
+  unsigned damaged=0;
+  while ((dg = iter.next())) {
+
+    Datagram& ddg = *reinterpret_cast<Datagram*>(dg);
+    CDatagram* odg = new(&pool) CDatagram(ddg,ddg.xtc);
+
+    events++;
+    inlet->post(odg);
+    sem.take();
+  }
+
+  close(fd);
+  return 0;
+}
