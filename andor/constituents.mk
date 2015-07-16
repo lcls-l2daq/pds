dc7d11e464fd395c2f5bdd1599dac0904096c1d5
jana <jana@SLAC.STANFORD.EDU>
2015-07-16 02:23:09 +0000

    Added temperature monitoring thread; write temperature to epics PV

--- andor/constituents.mk
+++ andor/constituents.mk
@@ -2,6 +2,6 @@ libnames := pdsandor andorutil
 
 libsrcs_pdsandor := $(wildcard *.cc)
 #libsinc_pdsandor := 
-libincs_pdsandor := pdsdata/include ndarray/include boost/include 
+libincs_pdsandor := pdsdata/include ndarray/include boost/include epics/include epics/include/os/Linux
 
 libsrcs_andorutil := AndorErrorCodes.cc
