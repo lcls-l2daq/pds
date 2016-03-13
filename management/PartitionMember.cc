367f95cdd9ca20691a4fb37e3acc38a6f878f2b5
weaver <weaver@SLAC.STANFORD.EDU>
2016-03-13 18:24:25 +0000

    Only receive occurrences when in the partition

--- management/PartitionMember.cc
+++ management/PartitionMember.cc
@@ -38,79 +38,79 @@ void PartitionMember::message(const Node& hdr, const Message& msg)
     case Message::Join:
     case Message::Alias:
       if (header().level()!=Level::Control) {
-  arpadd(hdr);
+        arpadd(hdr);
         Ins dst = msg.reply_to();
-  Message& rply = reply(msg.type());
+        Message& rply = reply(msg.type());
         ucast(rply, dst);
       }
       break;
     case Message::Transition:
       {
-  bool lpost = false;
-  bool lkill = false;
+        bool lpost = false;
+        bool lkill = false;
 
         const Transition& tr = reinterpret_cast<const Transition&>(msg);
-  if (tr.phase() == Transition::Execute &&
-      tr.id() == TransitionId::Map) {
-    if (_isallocated) break;
-    const Allocation& alloc =
-      reinterpret_cast<const Allocate&>(tr).allocation();
-    unsigned    nnodes = alloc.nnodes();
-    unsigned    index  = 0;
-    for (unsigned n=0; n<nnodes; n++) {
-      const Node* node = alloc.node(n);
-      if (node->level() == header().level()) {
-        if (header() == *node) {
-    _isallocated = true;
-    _allocator = hdr;
-    _occurrences = msg.reply_to();
-    allocated( alloc, _index=index );
-    lpost = true;
+        if (tr.phase() == Transition::Execute &&
+            tr.id() == TransitionId::Map) {
+          if (_isallocated) break;
+          const Allocation& alloc =
+            reinterpret_cast<const Allocate&>(tr).allocation();
+          unsigned    nnodes = alloc.nnodes();
+          unsigned    index  = 0;
+          for (unsigned n=0; n<nnodes; n++) {
+            const Node* node = alloc.node(n);
+            if (node->level() == header().level()) {
+              if (header() == *node) {
+                _isallocated = true;
+                _allocator = hdr;
+                _occurrences = msg.reply_to();
+                allocated( alloc, _index=index );
+                lpost = true;
+              }
+              index++;
+            }
+          }
         }
-        index++;
-      }
-    }
-  }
-  else if (tr.phase() == Transition::Execute &&
-     tr.id() == TransitionId::Unmap) {
-    const Kill& kill = reinterpret_cast<const Kill&>(tr);
-    if (_isallocated && kill.allocator() == _allocator) {
-      lkill = true;
-      lpost = true;
-    }
-  }
-  else
-    lpost = (_isallocated && hdr == _allocator);
+        else if (tr.phase() == Transition::Execute &&
+                 tr.id() == TransitionId::Unmap) {
+          const Kill& kill = reinterpret_cast<const Kill&>(tr);
+          if (_isallocated && kill.allocator() == _allocator) {
+            lkill = true;
+            lpost = true;
+          }
+        }
+        else
+          lpost = (_isallocated && hdr == _allocator);
 
-  if (lpost) {
+        if (lpost) {
           //    if (tr.id() != TransitionId::L1Accept) {
-      arpadd(hdr);
-      if (tr.phase() == Transition::Execute) {
-        Transition* ntr = new(&_pool) Transition(tr);
-        post(*ntr);
-      }
-      //  Initiate a datagram for all segment levels.
-      else if (header().level()==Level::Segment) {
-        CDatagram* ndg = new(&_pool) CDatagram(Datagram(tr,_contains,header().procInfo()));
-        if (tr.size() > sizeof(Transition) && _index==0) {
-          const Xtc& tc = *reinterpret_cast<const Xtc*>(&tr+1);
-          ndg->insert(tc,tc.payload());
+          arpadd(hdr);
+          if (tr.phase() == Transition::Execute) {
+            Transition* ntr = new(&_pool) Transition(tr);
+            post(*ntr);
+          }
+          //  Initiate a datagram for all segment levels.
+          else if (header().level()==Level::Segment) {
+            CDatagram* ndg = new(&_pool) CDatagram(Datagram(tr,_contains,header().procInfo()));
+            if (tr.size() > sizeof(Transition) && _index==0) {
+              const Xtc& tc = *reinterpret_cast<const Xtc*>(&tr+1);
+              ndg->insert(tc,tc.payload());
+            }
+            post(*ndg);
+          }
+          //    }
         }
-        post(*ndg);
-      }
-            //    }
-  }
-  if (lkill) {
-    _isallocated = false;
-    dissolved();
-  }
-  break;
+        if (lkill) {
+          _isallocated = false;
+          dissolved();
+        }
+        break;
       }
     default:
       break;
     }
   }
-  if (msg.type()==Message::Occurrence) {
+  if (msg.type()==Message::Occurrence && _isallocated) {
     Occurrence* occ = new(&_pool) Occurrence(reinterpret_cast<const Occurrence&>(msg));
     post(*occ);
   }
