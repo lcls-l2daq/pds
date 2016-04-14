c10c1868cb31f064a87ab35dfbb37d1ebd5da94b
caf <caf@SLAC.STANFORD.EDU>
2016-04-14 22:10:05 +0000

    back out copying of evgr directory from trunk

--- evgr/evgr/constituents.mk
+++
@@ -1,7 +0,0 @@
-libnames := evgr
-
-#libsrcs_evgr := $(filter-out EvrSimManager.cc,$(wildcard *.cc))
-#libsrcs_evgr := $(filter-out EvgManager.cc,$(wildcard *.cc))
-libsrcs_evgr := $(wildcard *.cc)
-libincs_evgr := evgr
-libincs_evgr += pdsdata/include ndarray/include boost/include 
