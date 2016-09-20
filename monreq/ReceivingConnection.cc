0b88eff8fb288e26ffe36720a6822d3a647c887c
weaver <weaver@SLAC.STANFORD.EDU>
2016-09-20 22:11:32 +0000

    Fix datagram header short read.  Fix indenting.

--- monreq/ReceivingConnection.cc
+++ monreq/ReceivingConnection.cc
@@ -25,107 +25,113 @@
 using namespace Pds;
 using namespace Pds::MonReq;
    
-      //
-      //  This is the connection for receiving datagrams on TCP "socket"
-      //
-      ReceivingConnection::ReceivingConnection(int socket, MonGroup* group): _socket(socket), _group(group), _numberRequested(0), _numberReceived(0) {
+//
+//  This is the connection for receiving datagrams on TCP "socket"
+//
+ReceivingConnection::ReceivingConnection(int socket, MonGroup* group): _socket(socket), _group(group), _numberRequested(0), _numberReceived(0) {
 
-	//
-	//Plot setups Number Requested/Received
-	//
-	MonDescScalar numberRequested_count("Number Requested");
-        _numberRequested_count = new MonEntryScalar(numberRequested_count);
-        _group->add(_numberRequested_count);
+  //
+  //Plot setups Number Requested/Received
+  //
+  MonDescScalar numberRequested_count("Number Requested");
+  _numberRequested_count = new MonEntryScalar(numberRequested_count);
+  _group->add(_numberRequested_count);
 
-	MonDescScalar numberReceived_count("Number Received");
-        _numberReceived_count = new MonEntryScalar(numberReceived_count);
-        _group->add(_numberReceived_count);
+  MonDescScalar numberReceived_count("Number Received");
+  _numberReceived_count = new MonEntryScalar(numberReceived_count);
+  _group->add(_numberReceived_count);
 
-	}
+}
 
-      //
-      //  Return the socket number for setting up poll
-      //
-      int ReceivingConnection::socket() const {
-	return _socket;
-	}
+//
+//  Return the socket number for setting up poll
+//
+int ReceivingConnection::socket() const {
+  return _socket;
+}
 
-      //
-      //  Make a new request
-      //
-      int ReceivingConnection::request() {
+//
+//  Make a new request
+//
+int ReceivingConnection::request() {
 
 
-		_numberRequested= _numberRequested +1; 
+  _numberRequested= _numberRequested +1; 
 		
-		//update plot value
-		_numberRequested_count->setvalue(_numberRequested);		
+  //update plot value
+  _numberRequested_count->setvalue(_numberRequested);		
 
-		if (send(_socket, (char*)&_numberRequested , sizeof(_numberRequested) , 0) < 0) {
-		perror("SEND TO REQUEST NUMBER: ");
-		return -1;
-		} 
-		timespec tv;
-		clock_gettime(CLOCK_REALTIME, &tv);
-			if(_numberReceived % 30 == 0) {
-			//printf("REQUEST_NUMBER, TIME: %i, %ld\n", _numberRequested, tv.tv_sec);
-			}
-		return 0;
-      }
+  if (send(_socket, (char*)&_numberRequested , sizeof(_numberRequested) , 0) < 0) {
+    perror("SEND TO REQUEST NUMBER: ");
+    return -1;
+  } 
+  timespec tv;
+  clock_gettime(CLOCK_REALTIME, &tv);
+  if(_numberReceived % 30 == 0) {
+    //printf("REQUEST_NUMBER, TIME: %i, %ld\n", _numberRequested, tv.tv_sec);
+  }
+  return 0;
+}
 
-      //
-      //  Receive a new event
-      //
-      char* ReceivingConnection::receive() {
+//
+//  Receive a new event
+//
+char* ReceivingConnection::receive() {
 
-		Pds::Dgram datagram;
-                         int nb = recv(_socket, &datagram, sizeof(datagram), 0) ;
+  Pds::Dgram datagram;
+  int nb = recv(_socket, &datagram, sizeof(datagram), MSG_WAITALL) ;
 		
-				if ( datagram.seq.isEvent() ) {
-				_numberReceived= _numberReceived + 1;
-				//printf("RECEIVED NUMBER: %i\n", _numberReceived);
+  if (nb==0) {
+    printf("ReceivingConnection::receive() socket closed\n");
+    return 0;
+  }
+  if  (nb == -1) {
+    perror("recv");
+    return 0;
+  }
+
+  //printf("%i [%d]\n", nb, datagram.xtc.sizeofPayload());
+
+  if (nb!=sizeof(datagram)) {
+    printf("Incomplete datagram header\n");
+    abort();
+  }
+
+  if ( datagram.seq.isEvent() ) {
+    _numberReceived= _numberReceived + 1;
+    //printf("RECEIVED NUMBER: %i\n", _numberReceived);
 				
-				//update plot value
-				_numberReceived_count->setvalue(_numberReceived);
-				_numberReceived_count->time(datagram.seq.clock());
-				_numberRequested_count->time(datagram.seq.clock());
+    //update plot value
+    _numberReceived_count->setvalue(_numberReceived);
+    _numberReceived_count->time(datagram.seq.clock());
+    _numberRequested_count->time(datagram.seq.clock());
 				
-					if(_numberReceived % 120 == 0) {
-					//printf("RECEIVED, TIME: %i %lf \n", _numberReceived, datagram.seq.clock().asDouble());
-					}
-				}
+    if(_numberReceived % 120 == 0) {
+      //printf("RECEIVED, TIME: %i %lf \n", _numberReceived, datagram.seq.clock().asDouble());
+    }
+  }
 
+  char* p = new char[sizeof(datagram)+datagram.xtc.sizeofPayload()];
+  memcpy(p, &datagram, sizeof(datagram));
+  char* q = p+sizeof(datagram);
+  int remaining = datagram.xtc.sizeofPayload();
+  do {
+    nb = recv(_socket, q, remaining, 0);
+    remaining -= nb;
+    q += nb;
+  } while (remaining>0);
+  return p;			
+}
 
-                         if (nb==0) return 0;
-                         	if  (nb == -1) {
-				perror("recv");
-				return 0;
-				}
-        		 //printf("%i [%d]\n", nb, datagram.xtc.sizeofPayload());
+//
+// Calulated ratio to determine 'best' to send requests to
+//
+int ReceivingConnection::ratio() const {
 
-         		char* p = new char[sizeof(datagram)+datagram.xtc.sizeofPayload()];
-         		memcpy(p, &datagram, sizeof(datagram));
-	 		char* q = p+sizeof(datagram);
-         		int remaining = datagram.xtc.sizeofPayload();
-         			do {
-           			nb = recv(_socket, q, remaining, 0);
-           			remaining -= nb;
-           			q += nb;
-         			} while (remaining>0);
-			return p;			
-	
-         
-	}
+  int r = _numberRequested - _numberReceived;
+  return r;
 
-	//
-	// Calulated ratio to determine 'best' to send requests to
-	//
-	int ReceivingConnection::ratio() const {
-
-	int r = _numberRequested - _numberReceived;
-	return r;
-
-	}
+}
 
 
 	
--- monreq/ServerConnection.cc
+++ monreq/ServerConnection.cc
@@ -12,76 +12,76 @@
 using namespace Pds;
 using namespace Pds::MonReq;
 
-      //
-      //  Track the connection to a requestor on TCP "socket"
-      //
-      ServerConnection::ServerConnection(int socket): _socket(socket), _numberRequested(0), _numberSent(0) {
-	Sockaddr k;	
-	socklen_t ka=k.sizeofName();
-	if( getpeername( _socket, k.name(), &ka) <0 ) {
-	perror("getpeername");
-	}
+//
+//  Track the connection to a requestor on TCP "socket"
+//
+ServerConnection::ServerConnection(int socket): _socket(socket), _numberRequested(0), _numberSent(0) {
+  Sockaddr k;	
+  socklen_t ka=k.sizeofName();
+  if( getpeername( _socket, k.name(), &ka) <0 ) {
+    perror("getpeername");
+  }
 	
 
-	_address = k.get().address();  
-      }
+  _address = k.get().address();  
+}
 
 
-      //
-      //  Return the socket number for setting up poll
-      //
-      int ServerConnection::socket() const {
-      return _socket;
-      }
+//
+//  Return the socket number for setting up poll
+//
+int ServerConnection::socket() const {
+  return _socket;
+}
 
 
-      int ServerConnection::address() const {
-      return _address;
-      }
+int ServerConnection::address() const {
+  return _address;
+}
 
-      //
-      //  Receive a new request
-      //
-      int ServerConnection::recv() {
+//
+//  Receive a new request
+//
+int ServerConnection::recv() {
 
-	 if(::recv(_socket, (char *)&_numberRequested, sizeof(_numberRequested), 0) < 0) {
-	 perror("receive number requested");
-	 return -1;
-	 }
-	 //printf("NUMBER OF REQUESTS RECEIVED: %i\n", _numberRequested);
-	 return 0;
-      }
+  if(::recv(_socket, (char *)&_numberRequested, sizeof(_numberRequested), 0) < 0) {
+    perror("receive number requested");
+    return -1;
+  }
+  //printf("NUMBER OF REQUESTS RECEIVED: %i\n", _numberRequested);
+  return 0;
+}
 
-      //
-      //  Fulfill an outstanding request, if necessary
-      //
-      int ServerConnection::send(const Datagram& dg) {
-
-
-		         //if we have any requests
-		         if (dg.seq.service()==TransitionId::L1Accept) { 
-
-				 if(_numberSent < _numberRequested) {
+//
+//  Fulfill an outstanding request, if necessary
+//
+int ServerConnection::send(const Datagram& dg) {
 
 
-		            	 // send an event
-		            	 int ca = ::send(_socket, &dg, sizeof(dg)+ dg.xtc.sizeofPayload(), MSG_NOSIGNAL );
-		            	 //printf("Datagram that is sending: %i\n", ca);
+  //if we have any requests
+  if (dg.seq.service()==TransitionId::L1Accept) { 
 
-				 //checking if sending correctly
-				 //printf("sizeofPayload: [%d]\n", dg.xtc.sizeofPayload());
+    if(_numberSent < _numberRequested) {
+
+
+      // send an event
+      int ca = ::send(_socket, &dg, sizeof(dg)+ dg.xtc.sizeofPayload(), MSG_NOSIGNAL );
+      //printf("Datagram that is sending: %i\n", ca);
+
+      //checking if sending correctly
+      //printf("sizeofPayload: [%d]\n", dg.xtc.sizeofPayload());
 		             
-				_numberSent = _numberSent +1;
-				//printf("REQUESTS FULLFILLED: %i\n", _numberSent);
-				}
-			}
-		        else {
-		         int ca = ::send(_socket, &dg, sizeof(Dgram)+ dg.xtc.sizeofPayload(), MSG_NOSIGNAL );
-		             //printf("%i\n", ca);
-		    	}
+      _numberSent = _numberSent +1;
+      //printf("REQUESTS FULLFILLED: %i\n", _numberSent);
+    }
+  }
+  else {
+    int ca = ::send(_socket, &dg, sizeof(Dgram)+ dg.xtc.sizeofPayload(), MSG_NOSIGNAL );
+    //printf("%i\n", ca);
+  }
 			
-		return 0;         
-	}
+  return 0;         
+}
 
 
 
