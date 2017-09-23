d9cdd20c6e45bec1c309867123d9cc16a6af9132
Daniel Damiani <ddamiani@slac.stanford.edu>
2017-09-22 20:30:11 -0700

    Add 'Endpoint' api for using libfabrics

--- eb/constituents.mk
+++ eb/constituents.mk
@@ -1,5 +1,5 @@
 libnames := eb
-tgtnames := ft_pingpong
+tgtnames := ft_pingpong ft_server ft_client
 
 #libsrcs_eb := $(wildcard *.cc)
 libsrcs_eb := fiTransport.cc
@@ -11,3 +11,11 @@ tgtincs_ft_pingpong := pdsdata/include ndarray/include boost/include
 tgtincs_ft_pingpong += libfabric/include pds/eb
 tgtlibs_ft_pingpong := libfabric/fabric pds/eb
 tgtslib_ft_pingpong := pthread
+
+tgtsrcs_ft_server	:= ft_server.cc Endpoint.cc
+tgtincs_ft_server := libfabric/include pds/eb
+tgtlibs_ft_server := libfabric/fabric pds/eb
+
+tgtsrcs_ft_client := ft_client.cc Endpoint.cc
+tgtincs_ft_client := libfabric/include pds/eb
+tgtlibs_ft_client := libfabric/fabric pds/eb
